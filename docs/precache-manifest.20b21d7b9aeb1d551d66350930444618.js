9fd4488dd75d42562edf0ba3eda29d282bf9f11b
jovaslink <jovaslink@gmail.com>
2020-12-12 01:56:13 -0600

    docs

---
+++ docs/precache-manifest.20b21d7b9aeb1d551d66350930444618.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599135f00cccb14360042ad5d88569e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0e7df25083817531580",
+    "url": "/static/css/main.a874d254.chunk.css"
+  },
+  {
+    "revision": "16e03aeff7401e4aabd3",
+    "url": "/static/js/2.294566df.chunk.js"
+  },
+  {
+    "revision": "dad1e10c751afc1e6fffeb8be63f9aab",
+    "url": "/static/js/2.294566df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e7df25083817531580",
+    "url": "/static/js/main.55292045.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
